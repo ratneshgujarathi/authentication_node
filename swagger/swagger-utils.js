f62406c8f9d3c5c781521ee3cf7cd000d9f5a820
Ratnesh Gujarathi <ratneshgujarathi@outlook.com>
2024-02-23 19:48:04 +0530

    Get Swagger Component Overload Added

--- swagger/swagger-utils.js
+++ swagger/swagger-utils.js
@@ -1,5 +1,6 @@
-const getSwaggerComponents = (collection, options=[]) => {
-    return collection.filter(item => options.includes(item.name))
+let getSwaggerComponents = (collection, options = [], required = false) => {
+    return required ? collection.filter(item => options.includes(item.name) && item.required == true) 
+    : collection.filter(item => options.includes(item.name));
 }
 
-module.exports = {getSwaggerComponents}
+module.exports = { getSwaggerComponents }
